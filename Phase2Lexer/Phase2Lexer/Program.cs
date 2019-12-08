ca2677fb8702a19d7f1557ea541b93628792b02f
syedbasit123 <46702821+syedbasit123@users.noreply.github.com>
2019-12-08 20:45:08 +0500

    Create Program.cs

---
+++ Phase2Lexer/Phase2Lexer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase2Lexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string text = File.ReadAllText("pythonFile.txt");
+                Lexer lex = new Lexer();
+                lex.Parse(text);
+
+                while (text != null)
+                {
+                    text = text.Trim(' ', '\t');
+                    string token = lex.GetNextLexicalAtom(ref text);
+                    Console.Write(token);
+                }
+            }
+            catch (Exception)
+            {
+
+               
+            }
+           
+
+        }
+    }
+}
